366dc721ff606ecbf33013c6ab373c8a98b24660
Ryan Martin <ryanmartinneutrino@gmail.com>
2015-09-12 18:33:38 -0400

    updated new question form to deal with multiple answers nicely

--- client/templates/newQuestion.js
+++ client/templates/newQuestion.js
@@ -1,4 +1,8 @@
 
+Template.newQuestion.onCreated(function(){
+  Session.set("nAnswers",1);
+});
+
 Template.newQuestion.helpers({
 
   loggedIntoCourse: function(){
@@ -26,6 +30,20 @@ Template.newQuestion.helpers({
     var currentUser=Meteor.userId();
     return Courses.find({$or:[{createdBy_id:currentUser},{enrolledUserIds:currentUser}]});
   },
+  moreThanOneAnswer:function(){
+    var nAnswers=Session.get("nAnswers");
+    if(nAnswers>1)return true;
+    else return false;
+  },
+  answers:function(){
+    var answerArray = [];
+    var nAnswers=Session.get("nAnswers");
+    var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    for (var i=0;i<nAnswers;i++){
+      answerArray.push({id:"ans_"+i,letter:alphabet[i]});
+    }
+    return answerArray;  
+  }
 
 });
 
@@ -34,7 +52,27 @@ Template.newQuestion.events({
   "change .selectCourse":function(event){
     event.preventDefault();
     Session.setPersistent("currentCourse",$('[id=selectCourse]').val());
+  },
+ 
+  "ondrop .upload-drop-zone":function(event){
+    event.preventDefault();
+    console.log(event.dataTransfer.files);
+  },
+
+  "click .addAnswerButton":function(event){
+    event.preventDefault();
+    var nAnswers=Session.get("nAnswers")+1;
+    Session.set("nAnswers",nAnswers);
+  },
+
+  "click .removeAnswerButton":function(event){
+    event.preventDefault();
+    var nAnswers=Session.get("nAnswers")-1;
+    if(nAnswers<1)return;
+    Session.set("nAnswers",nAnswers);
   }
+
+
 });
 
 
